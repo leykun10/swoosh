413c1617122009a69b4b7358930016b25b9bc82c
unknown <leykunejigue@gmail.com>
2020-12-04 12:56:10 +0300

    lesson04

--- app/src/main/java/com/example/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/LeagueActivity.kt
@@ -4,14 +4,49 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+     var selectedLeague:String?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+
+
     }
     fun leagueNextClicked(view:View){
-        val skillActivity  = Intent(this,SkillActivity::class.java)
-        startActivity(skillActivity)
+        if(selectedLeague!=null){
+            val skillActivity  = Intent(this,SkillActivity::class.java)
+            skillActivity.putExtra(
+                ExtraLeague,selectedLeague
+            )
+            startActivity(skillActivity)
+        }
+        else{
+        Toast.makeText(applicationContext,"select a league",Toast.LENGTH_SHORT).show()
+
+        }
+
     }
+
+    fun womenButtonClicked(view:View){
+            selectedLeague="womenLeague"
+            menButton.isChecked=false
+            coedButton.isChecked=false
+    }
+    fun menButtonClicked(view:View){
+        selectedLeague="menLeague"
+        womenButton.isChecked=false
+        coedButton.isChecked=false
+
+    }
+    fun coedButtonClicked(view:View){
+        selectedLeague="coedLeague"
+        menButton.isChecked=false
+        womenButton.isChecked=false
+
+    }
+
 }
--- app/src/main/java/com/example/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -3,10 +3,13 @@ package com.example.swoosh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+    println(intent.getStringExtra(ExtraLeague))
     }
 
 }
